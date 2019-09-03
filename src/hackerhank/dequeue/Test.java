c907c169f93a2e8cf6d9a1e5bb3bee798e87e37a
samueel <samueel@gmail.com>
2019-09-03 06:13:50 -0300

    Versão final

--- src/hackerhank/dequeue/Test.java
+++ src/hackerhank/dequeue/Test.java
@@ -1,112 +1,91 @@
 package hackerhank.dequeue;
 
-import java.time.Instant;
+
+
 import java.util.ArrayDeque;
-import java.util.ArrayList;
 import java.util.Deque;
-import java.util.HashSet;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
-import java.util.Set;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class Test {
-    private static final int MIN_LIM = 0;
-    private static final int MAX_PARAM_LIM = 100000;
-    private static final int MAX_NUM_LIM = 10000000;
+	private static final int MIN_LIM = 0;
+	private static final int MAX_PARAM_LIM = 100000;
+	private static final int MAX_NUM_LIM = 10000000;
 
-    /**
-    * Validate the Entries
-    */
-    private static void validateEntry (int n, int m) {
+	/**
+	 * Validate the Entries
+	 */
+	private static void validateEntry(int n, int m) {
 
-        if ((n < MIN_LIM && n > MAX_PARAM_LIM) ||
-            (m < MIN_LIM && m > MAX_PARAM_LIM)){
-                throw new IllegalArgumentException ("Invalid values for params");
-            }
-    }
+		if ((n < MIN_LIM && n > MAX_PARAM_LIM) || (m < MIN_LIM && m > MAX_PARAM_LIM)) {
+			throw new IllegalArgumentException("Invalid values for params");
+		}
+	}
 
-    /**
-    * Validate the elements
-    */
-    private static void validateArrayElement (int num) {
+	/**
+	 * Validate the elements
+	 */
+	private static void validateArrayElement(int num) {
 
-        if (num < MIN_LIM && num > MAX_NUM_LIM){
-            throw new IllegalArgumentException ("Invalid values for elements");
-        }
-    }
+		if (num < MIN_LIM && num > MAX_NUM_LIM) {
+			throw new IllegalArgumentException("Invalid values for elements");
+		}
+	}
 
-    /**
-        Prepare de Sub-arrays
-    */
-    private static Deque<Deque> fillDeck (Deque deckReturn, Deque fullDeck, int m) {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		Deque<Integer> deque = new ArrayDeque<>();
+		// amount element
+		int n = in.nextInt();
+		// subarray size
+		int m = in.nextInt();
 
-        Object[] subArray = fullDeck.toArray();
-        Deque subDeck = new ArrayDeque<>();
+		validateEntry (n,m);
+		Map<Integer,Integer> mapa = new HashMap<>();
+		int maxValue = -1;
 
-        if (fullDeck.size() >= m) {
+		for (int i = 0; i < n; i++) {
+			int num = in.nextInt();
+			validateArrayElement (num);
+			if (deque.size() == m) {
+				// O elemento removed sempre estará no Mapa
+				//Processa a remoção
+				Integer removed = deque.removeFirst();
+				Integer ocorrenciaRemoved = mapa.get(removed);
+				if (ocorrenciaRemoved > 1) {
+					mapa.put (removed, mapa.get(removed)-1);
+				} else {
+					mapa.remove(removed);
+				}
+				//Fim remoção
+				
+				//Processa inclusão
+				deque.addLast(num);
+				updateMap(mapa, num);
+				//Fim inclusão
+			} else {
+				deque.addLast(num);
+				updateMap(mapa, num);
+			}
+			
+			if (mapa.size() > maxValue) {
+				maxValue = mapa.size();
+			}
 
-            for (int i =0;i<m;i++){
-                subDeck.offerLast(subArray[i]);
-            }
+		}
 
-            fullDeck.remove();
-            deckReturn.addLast (subDeck);
-            fillDeck (deckReturn, fullDeck, m); 
-        } 
-        
-        return deckReturn;
+		System.out.println(maxValue);
 
-    }
+		in.close();
+	}
 
-    /**
-    * Find the quantity of unique numbers by Sub-array
-    */
-    private static int findQuantityUniqueNumbers (Deque deque) {
-        
-        Set<Integer> numbers = new HashSet<>();
-        numbers.addAll(deque);
-
-        return numbers.size();
-    }
-
-    public static void main(String[] args) {
-    	System.out.println("Inicio: "+ Instant.now());
-    	Scanner in = new Scanner(System.in);
-        Deque deque = new ArrayDeque<>();
-        //amount element
-        int n = in.nextInt();
-        //subarray size
-        int m = in.nextInt();
-        final AtomicInteger maxUniqueNumber = new AtomicInteger (-1);
-
-        validateEntry (n,m);
-
-        for (int i = 0; i < n; i++) {
-            int num = in.nextInt();
-            validateArrayElement (num);
-            deque.offerLast (num);
-        }
-
-        Deque decks = fillDeck(new ArrayDeque<>(), deque,m);
-
-        List<Deque> sub = new ArrayList<>();
-        sub.addAll (decks);
-
-        sub.forEach ( subArray -> {
-            int quant = findQuantityUniqueNumbers (subArray);
-
-            if (quant > maxUniqueNumber.get()) {
-               maxUniqueNumber.set (quant);
-            }
-
-        });
-
-        System.out.println (maxUniqueNumber);
-    	System.out.println("Fim: "+ Instant.now());
-
-    }
+	private static void updateMap(Map<Integer, Integer> mapa, int num) {
+		Integer ocorrencia = mapa.get(num);
+		if (ocorrencia == null) {
+			mapa.put(num,1);
+		} else {
+			mapa.put (num, mapa.get(num)+1 );
+		}
+	}
 }
-
-
-
